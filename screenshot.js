578f340b2a59f202f7162db39829f3ad3881edeb
Radu Dan <radu@full-throttle.ro>
2017-11-11 16:16:42 +0100

    Add screenshot service

---
+++ screenshot.js
@@ -0,0 +1,42 @@
+const bluebird = require("bluebird");
+const puppeteer = require("puppeteer");
+const ms = require("ms");
+
+const INTERVAL = ms("30m");
+async function main() {
+    const browser = await puppeteer.launch();
+    try {
+        while(true) {
+            const page = await browser.newPage();
+            try {
+                page.setViewport({width: 1400, height: 730});
+                await page.goto("http://localhost:5000");
+                await page.screenshot({
+                    path: "static/cover.jpeg",
+                    type: "jpeg",
+                    quality: 60,
+                    clip: {
+                        x: 50,
+                        y: 50,
+                        width: 1200,
+                        height: 630
+                    }
+                });
+            } finally {
+                await page.close();
+                await bluebird.delay(INTERVAL);
+            }
+        }
+    } finally {
+        await browser.close();
+    }
+}
+
+main()
+.then(() => {
+    process.exit(0);
+})
+.catch((err) => {
+    console.error(err.stack);
+    process.exit(1);
+});
